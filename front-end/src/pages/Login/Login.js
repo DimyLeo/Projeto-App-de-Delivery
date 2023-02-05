1c9bafb3017a68b175d297c9c2de4d36bbf2f53a
DimyLeo <leonardoprimoc@hotmail.com>
2023-02-05 16:06:51 -0300

    test login

--- front-end/src/pages/Login/Login.js
+++ front-end/src/pages/Login/Login.js
@@ -14,7 +14,7 @@ const STYLE_CLASSNAMES = {
 const BACKEND_PORT = 3001;
 const SUCCESS = 200;
 
-const baseUrl = process.env.BASE_URL || 'http://localhost:3001';
+const baseUrl = process.env.BASEURL || 'http://localhost:3001';
 
 function Login() {
   const [isError, setIsError] = useState([]);
