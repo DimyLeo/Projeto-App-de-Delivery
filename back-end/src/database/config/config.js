2d9bda5ee14b2f12f6143e27064be0bd02fbf6aa
DimyLeo <leonardoprimoc@hotmail.com>
2023-02-05 15:24:51 -0300

    config

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -1,27 +1,12 @@
 require('dotenv').config();
 
-// const environment = process.env.NODE_ENV || "test";
-
-// const suffix = {
-//   prod: "",
-//   production: "",
-//   dev: "-dev",
-//   development: "-dev",
-//   test: "-test",
-// };
-
 const options = {
-  host: process.env.HOSTNAME || process.env.MYSQLHOST || 'localhost',
+  host: process.env.MYSQLHOST || 'localhost',
   port: process.env.MYSQLPORT || '3306',
-  database: 
-    `${process.env.MYSQLDATABASE || 'delivery-app'}`,
+  database: process.env.MYSQLDATABASE || 'delivery-app',
   username: process.env.MYSQLUSER || 'root',
   password: process.env.MYSQLPASSWORD || 'password',
   dialect: 'mysql',
-  dialectOptions: {
-    timezone: 'Z',
-  },
-  logging: false,
 };
 
 module.exports = {
